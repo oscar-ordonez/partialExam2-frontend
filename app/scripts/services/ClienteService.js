9e5f2584d3224e42ae57276cd354793341c9989f
Oscar Ordoñez <Oscar Ordoez>
2016-03-30 17:53:44 -0600

    Frontend

---
+++ app/scripts/services/ClienteService.js
@@ -0,0 +1,25 @@
+angular.module('AngularScaffold.Services').factory('ClienteService', ['$http',
+	function($http){
+		$http.defaults.withCredentials = true;
+		var baseUrl = 'https://oscar-ordonez-backend.herokuapp.com/';
+		return {
+				UpdateUser: function(param,payload){
+          return $http.post(baseUrl + "v1/updateUser/"+ param, payload);
+        },
+				DeleteUser: function(params){
+          return $http.delete(baseUrl + "v1/deleteUser/"+ params);
+        },
+				GetUser: function(params){
+					return $http.get(baseUrl + "v1/getUser/"+ params);
+        },
+				GetUsers: function(){
+					return $http.get(baseUrl + "v1/users");
+				},
+				Logout: function(){
+					return $http.get(baseUrl + "v1/logout");
+				},
+        Register: function(payload){
+          return $http.post(baseUrl + "v1/register", payload);
+        }
+	    };
+}]);
